1de58da955fff3ab603466e684b8e64ea864d828
Alonso <alonso12_284@outlook.com>
2021-03-17 19:02:34 -0600

    Add validateString a function

--- main.py
+++ main.py
@@ -1,35 +1,38 @@
-#Welcome, to the TI club discord bot code.
+# Welcome, to the TI club discord bot code.
 
-#introduction
+# introduction
 
 """ mac
 python3 -m pip install -U discord.py
 windows
 py -3 -m pip install -U discord.py """
 
-#Import discord library and logging library
-import discord, logging
+# Import discord library and logging library
+import discord
+import logging
 #import intents
 from discord.ext import commands
-#Import JSON
+# Import JSON
 import json
 
-#Set up logging
+# Set up logging
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+handler = logging.FileHandler(
+    filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter(
+    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-#setup intents
+# setup intents
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
 
 # client = discord.Client()
-bot = commands.Bot(command_prefix = '?')
+bot = commands.Bot(command_prefix='$')
 
-#fix boot
+# fix boot
 """ #boot up
 @bot.event
 async def on_ready():
@@ -47,23 +50,21 @@ async def on_ready():
 with open('./magIAObject.json') as myObject:
     magIA = json.load(myObject)
 
+
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
 
+
 @bot.command()
 async def getCode(ctx, modulo, dia):
-    await ctx.send('El código es {}'.format(magIA[modulo][dia]))
 
-@bot.command()
-async def setCode(ctx, modulo, dia, codigo):
-
-    #modulo no existe
+    # modulo no existe
     if(not(modulo in magIA)):
         await ctx.send('Ese módulo no existe, porfavor intente de nuevo')
         return
 
-    #formato de dia es incorrecto
+    # formato de dia es incorrecto
     try:
         int(dia[0])
         int(dia[1])
@@ -71,17 +72,42 @@ async def setCode(ctx, modulo, dia, codigo):
         await ctx.send('Tu formato de dia es incorrecto, porfavor intenta de nuevo')
         return
 
-    #formato de mes es incorrecto
-    if(not(dia[2:]=='marzo' or dia[2:]=='abril' or dia[2:]=='mayo')):
+    # formato de mes es incorrecto
+    if(not(dia[2:] == 'marzo' or dia[2:] == 'abril' or dia[2:] == 'mayo')):
         await ctx.send('Tu formato de mes es incorrecto, porfavor intenta de nuevo')
         return
 
-    #El codigo ya existe
+    await ctx.send('El código es {}'.format(magIA[modulo][dia]))
+    return
+
+
+@bot.command()
+async def setCode(ctx, modulo, dia, codigo):
+
+    # modulo no existe
+    if(not(modulo in magIA)):
+        await ctx.send('Ese módulo no existe, porfavor intente de nuevo')
+        return
+
+    # formato de dia es incorrecto
+    try:
+        int(dia[0])
+        int(dia[1])
+    except:
+        await ctx.send('Tu formato de dia es incorrecto, porfavor intenta de nuevo')
+        return
+
+    # formato de mes es incorrecto
+    if(not(dia[2:] == 'marzo' or dia[2:] == 'abril' or dia[2:] == 'mayo')):
+        await ctx.send('Tu formato de mes es incorrecto, porfavor intenta de nuevo')
+        return
+
+    # El codigo ya existe
     if(dia in magIA[modulo]):
         await ctx.send('Este código ya fue almacenado, para acceder a él usa el comando `$getCode`')
         return
 
-    #almacenar código
+    # almacenar código
     magIA[modulo][dia] = codigo
     with open('./magIAObject.json', 'w') as myObject:
         json.dump(magIA, myObject)
@@ -89,7 +115,14 @@ async def setCode(ctx, modulo, dia, codigo):
     return
 
 
+@bot.command()
+async def getLink(ctx):
+    await ctx.send(content='Link de registro <http://registromagiajuvenil.southcentralus.cloudapp.azure.com/>', embed=None)
 
+
+@bot.command()
+async def contactInfo(ctx):
+    await ctx.send(content='contacto_magiajuvenil@frskills.com')
 
 
 bot.run('')
